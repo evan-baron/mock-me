305bcc2a707a221c4e54c0cd17c749baeebdb851
evan baron <egbaron@gmail.com>
2023-06-17 02:21:06 -0600

    removing unnecessary code

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function mockify(text) {
     let arr = text.split('');
     const notLetters = [];
     const notLettersIndex = [];
+
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].match(/[a-zA-Z]/)) {
         } else {
@@ -41,24 +42,20 @@ function mockify(text) {
             notLetters.push(arr[i]);
         }
     }
-    console.log(arr);
-    console.log(notLetters);
-    console.log(notLettersIndex);
+
     let filter = arr.filter(checkSpace);
     function checkSpace(val) {
         return val.match(/[a-zA-Z]/)
     }
-    console.log('checking for characters ' + filter.join(''));
+
     for (let i = 0; i<filter.length; i+=2) {
         let string = filter[i];
         filter.splice(i, 1, string.toUpperCase());
     }
-    console.log(filter.join(''));
+
     for (let i = 0; i < notLettersIndex.length; i++) {
         filter.splice(notLettersIndex[i], 0, notLetters[i]);
     }
-    console.log(filter.join(''));
-    console.log(notLettersIndex);
 
     document.getElementById('mockIn').value = filter.join('');
 
@@ -67,10 +64,6 @@ function mockify(text) {
     copyText.setSelectionRange(0, 9999);
     navigator.clipboard.writeText(copyText.value);
     copyText.blur();
-}
-
-function test() {
-    console.log(document.getElementById('mockIn').value);
 }
 
 document.getElementById('mockIn').addEventListener('click', function() {
