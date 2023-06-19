05317e3179e5b07fec1727a2112312cac3191142
evan baron <egbaron@gmail.com>
2023-06-19 12:04:16 -0600

    added comments to javascript

--- script.js
+++ script.js
@@ -29,11 +29,19 @@ function mockifyNoNums(text) {
 }
 
 function mockify(text) {
+    //make everything lowercase
     text = document.getElementById('mockIn').value.toLowerCase();
+
+    //splitting up the textarea value into elements in an array
     let arr = text.split('');
+
+    //array of symbols
     const notLetters = [];
+
+    //array of symbols indexes
     const notLettersIndex = [];
 
+    //checking the textarea value for symbols and pushing them to their associated arrays
     for (let i = 0; i < arr.length; i++) {
         if (!arr[i].match(/[a-zA-Z]/)) {
             notLettersIndex.push(i);
@@ -41,26 +49,32 @@ function mockify(text) {
         }
     }
 
+    //putting all the letters into a new array in order
     let newText = arr.filter((val) => {
         return val.match(/[a-zA-Z]/)
     });
 
+    //uppercasing every other element in newText array
     for (let i = 0; i<newText.length; i+=2) {
         let string = newText[i];
         newText.splice(i, 1, string.toUpperCase());
     }
 
+    //adding the symbols back into the string in their exact order
     for (let i = 0; i < notLettersIndex.length; i++) {
         newText.splice(notLettersIndex[i], 0, notLetters[i]);
     }
 
+    //outputting the string back into textarea value
     document.getElementById('mockIn').value = newText.join('');
 
+    //copying the string to clipboard
     let copyText = document.getElementById('mockIn');
     navigator.clipboard.writeText(copyText.value);
     copyText.blur();
 }
 
+//clear the textarea on focus
 function clearText() {
     console.log('test');
     document.getElementById('mockIn').value = '';
