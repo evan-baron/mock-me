8ef6d9936a961499fb3f343afdffd492025d97db
evan baron <egbaron@gmail.com>
2024-05-20 10:04:59 -0600

    removed unused code

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// Below is the function mockify before checking for numbers in the string
-function mockifyNoNums(text) {
-    console.log(text);
-    let arr = text.split('');
-    const spaces = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === ' ') {
-            spaces.push(i);
-        }
-    }
-    console.log(arr);
-    let filter = arr.filter(checkSpace);
-    function checkSpace(val) {
-        return val != ' ';
-    }
-    console.log(filter.join(''));
-    for (let i = 0; i<filter.length; i+=2) {
-        let string = filter[i];
-        filter.splice(i, 1, string.toUpperCase());
-    }
-    console.log(filter.join(''));
-    for (let i = 0; i < spaces.length; i++) {
-        filter.splice(spaces[i], 0, ' ');
-    }
-    console.log(filter.join(''));
-    console.log(spaces);
-
-    return filter.join('');
-}
-
 function mockify(text) {
     //make everything lowercase
     text = document.getElementById('mockIn').value.toLowerCase();
